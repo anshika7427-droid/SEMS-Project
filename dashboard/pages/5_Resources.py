cf6cb15ac95bbe5bf437afde602491b09e2d704e
Anshika <anshika7427@gmail.com>
2026-06-12 01:06:54 +0530

    Initial commit of SEMS Project

--- dashboard/pages/5_Resources.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.title("📚 Resources")
-
-uploaded_file = st.file_uploader("Upload Notes/PDF")
-
-if uploaded_file:
-    st.success("File uploaded successfully")
--- dashboard/pages/6_Profile.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.title("👤 Profile")
-
-st.text_input("Name")
-
-st.text_input("Email")
-
-st.button("Save Profile")
